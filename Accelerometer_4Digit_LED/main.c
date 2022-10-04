fd5929fe95b029a92782abb51afcc73583fd708c
Gandhar Deshpande <gandhard1@gmail.com>
2022-10-03 20:45:20 -0400

    Update main.c

--- Accelerometer_4Digit_LED/main.c
+++ Accelerometer_4Digit_LED/main.c
@@ -1,19 +1,19 @@
 #include <msp430.h>
 
 // PORT2 pins for turning on LED digits
-#define DIGIT_1 0x08  //p2.3
-#define DIGIT_2 0x04  //p2.2
-#define DIGIT_3 0x02  //p2.1
-#define DIGIT_4 0x01  //p2.0
+#define DIGIT_1 BIT3  //p2.3
+#define DIGIT_2 BIT2  //p2.2
+#define DIGIT_3 BIT1  //p2.1
+#define DIGIT_4 BIT0  //p2.0
 
 // 7 segments B-F of LED connected to pins of port1 and  A to port2
-#define a 0x10    //p2.4
-#define b 0x20    //p1.5
-#define c 0x08    //p1.3
-#define d 0x02    //p1.1
-#define e 0x01    //p1.0
-#define f 0x40    //p1.6
-#define g 0x10    //p1.4
+#define a BIT4    //p2.4
+#define b BIT5    //p1.5
+#define c BIT6    //p2.6
+#define d BIT7    //p2.7
+#define e BIT0    //p1.0
+#define f BIT6    //p1.6
+#define g BIT4    //p1.4
 
 //Digit dots on LED
 #define DOT BIT5 //p2.5
@@ -22,6 +22,9 @@
 #define LED_DIGITS 4
 
 #define DIGDELAY 1000 //Number of cycles to delay for displaying each digit in display_digits
+
+// to track which digits of led are on
+unsigned char digits_on = 0x00;
 
 typedef enum
 {
@@ -32,6 +35,7 @@ typedef enum
     E_SEGMENT,
     F_SEGMENT,
     G_SEGMENT,
+    DP
 } LED_SEGMENTS;
 
 //p1.7 for ADC
@@ -48,26 +52,15 @@ void configureAdc();
 //Reads ADC_INPUT of ADC and Returns the digital Value as an integer
 //*******************************************************************************
 unsigned int read_adc();
-//******************************************************************************
-//Module Function read_adc_mean_filtered(), Last Revision date 9/22/2022, by Owen
-//Given a number, reads the ADC a certain number of times, and returns the unweighted average
-//Ultimately not used do to combination of time and oscillation
-//*******************************************************************************
-unsigned int read_adc_mean_filtered(volatile unsigned int number);
 
-//******************************************************************************
-//Module Function read_adc_median_filtered(), Last Revision date 9/22/2022, by Owen
-//Given a number, reads the ADC a certain number of times, and returns the median of them
-//Ultimately not used do to combination of time and oscillation
-//*******************************************************************************
-unsigned int read_adc_median_filtered(volatile unsigned int number);
 
 //******************************************************************************
 //Module Function read_adc_running_average_filter(), Last Revision date 9/22/2022, by Owen
 //Taking the old average, and the weight, which is set above, calculates the new running average
 //The higher the weight, the more oscillations are damped down
 //*******************************************************************************
-unsigned int read_adc_running_average_filter(unsigned int oldaverage, unsigned int weight);
+unsigned int read_adc_running_average_filter(unsigned int oldaverage,
+                                             unsigned int weight);
 
 //******************************************************************************
 //Module Function led_display_num(), Last Revision date 9/22/2022, by Gandhar
@@ -83,15 +76,19 @@ void led_display_num(unsigned val);
 //*******************************************************************************
 void lit_led_segment(LED_SEGMENTS segment);
 
-void display_digits(unsigned int val);
+void display_digits(unsigned int val, char axis);
 
+void turn_off_led_segments();
 
 void main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;       // stop watchdog timer
 
-    P2DIR |= DIGIT_1 + DIGIT_2 + DIGIT_3 + DIGIT_4 + a + DOT;
-    P1DIR |= b + c + d + e + f + g ;
+    P2DIR |= DIGIT_1 + DIGIT_2 + DIGIT_3 + DIGIT_4 + a + c + d + DOT;
+    P1DIR |= b + e + f + g;
+
+    P2SEL &= ~BIT6;  //enable p2.6 as gpio
+    P2SEL &= ~BIT7;  //enable p2.7 as gpio
 
     volatile unsigned int i = 0;
     unsigned int oldaverage = 0;
@@ -99,15 +96,31 @@ void main(void)
     configureAdc();
     unsigned int adc_val = read_adc();
     oldaverage = adc_val;
-    display_digits(adc_val); //The problem with Running Average is that it there can be a "ramp up", displaying one value in beginning helps mitigate this
+    display_digits(adc_val, 'x'); //The problem with Running Average is that it there can be a "ramp up", displaying one value in beginning helps mitigate this
     while (1)
     {
 
+        digits_on = 0x00;
 //        unsigned int adc_val = read_adc_median_filtered();
         unsigned int adc_val = read_adc();
         adc_val = read_adc_running_average_filter(oldaverage, 10);
-        display_digits(adc_val);
+        display_digits(adc_val, 'z');
         oldaverage = adc_val;
+
+
+//        P2OUT = DIGIT_1;
+//        P2OUT &= ~DOT;
+//        _delay_cycles(1000000);
+//        P2OUT = DIGIT_2;
+//        P2OUT &= ~DOT;
+//        _delay_cycles(1000000);
+//        P2OUT = DIGIT_3;
+//        P2OUT &= ~DOT;
+//        _delay_cycles(1000000);
+//        P2OUT = DIGIT_4;
+//        P2OUT &= ~DOT;
+//        _delay_cycles(1000000);
+//        display_digits(9,'x');
     }
 }
 
@@ -134,57 +147,28 @@ unsigned int read_adc()
     unsigned int ADC_value = ADC10MEM; // Assigns the value held in ADC10MEM to the integer called ADC_value
     return ADC_value;
 }
-//******************************************************************************
-//Module Function read_adc_mean_filtered(), Last Revision date 9/22/2022, by Owen
-//Given a number, reads the ADC a certain number of times, and returns the unweighted average
-//Ultimately not used do to combination of time and oscillation
-//*******************************************************************************
-unsigned int read_adc_mean_filtered(volatile unsigned int number) {
-    unsigned int values = 0;
-    volatile unsigned int i;
-    for(i = number; i > 0; i--) {
-        values += read_adc();
-    }
-    return values / number;
-}
 
-//******************************************************************************
-//Module Function read_adc_median_filtered(), Last Revision date 9/22/2022, by Owen
-//Given a number, reads the ADC a certain number of times, and returns the median of them
-//Ultimately not used do to combination of time and oscillation
-//*******************************************************************************
-unsigned int read_adc_median_filtered(volatile unsigned int number)
-{
-    unsigned int arr[number];
-    volatile unsigned int i,j,tmp;
 
-    for(i = number; i > 0; i--) { //reads ADC certain number of times
-        arr[i] = read_adc();
-    }
 
-    for(i=0;i<number;i++) //Algorithm taken from robotics class last semester
-    {                     //Sorts the array by size
-        for(j=0; j < number; j++)
-        {
-            if(arr[j] > arr[i]) {
-                tmp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = tmp;
-            }
-        }
-    }
-    return arr[number / 2]; //returns the middle value in the array, which is the Median
-}
 //******************************************************************************
 //Module Function read_adc_running_average_filter(), Last Revision date 9/22/2022, by Owen
 //Taking the old average, and the weight, which is set above, calculates the new running average
 //The higher the weight, the more oscillations are damped down
 //*******************************************************************************
-unsigned int read_adc_running_average_filter(unsigned int oldaverage, unsigned int weight) {
+unsigned int read_adc_running_average_filter(unsigned int oldaverage,
+                                             unsigned int weight)
+{
     unsigned int average;
     unsigned int value = read_adc();
-    average =  (oldaverage * weight + value)/(weight + 1); //Simple running average formula
+    average = (oldaverage * weight + value) / (weight + 1); //Simple running average formula
     return average;
+}
+
+void turn_off_led_segments()
+{
+    // turn off all segments first
+    P1OUT |= b + e + f + g;
+    P2OUT |= a + c + d + DOT;
 }
 
 //******************************************************************************
@@ -194,9 +178,7 @@ unsigned int read_adc_running_average_filter(unsigned int oldaverage, unsigned i
 //*******************************************************************************
 void led_display_num(unsigned val)
 {
-    // turn off all segments first
-    P1OUT |= b + c + d + e + f + g;
-    P2OUT |= a + DOT;
+    turn_off_led_segments();
     //For each number, light the needed segments using program lit_let_segment
     switch (val)
     {
@@ -316,12 +298,12 @@ void lit_led_segment(LED_SEGMENTS segment)
     }
     case C_SEGMENT:
     {
-        P1OUT &= ~c;
+        P2OUT &= ~c;
         break;
     }
     case D_SEGMENT:
     {
-        P1OUT &= ~d;
+        P2OUT &= ~d;
         break;
     }
     case E_SEGMENT:
@@ -339,8 +321,46 @@ void lit_led_segment(LED_SEGMENTS segment)
         P1OUT &= ~g;
         break;
     }
+    case DP:
+    {
+        P2OUT &= ~DOT;
+    }
     default:
         break;
+    }
+}
+
+void display_decimal_point(char axis, unsigned char on_digits)
+{
+    if (axis == 'x')
+    {
+        if (!(on_digits & (1 << 3))) //check if digit-1 is turned off and then turn on to show dp
+        {
+            P2OUT = DIGIT_1;
+            turn_off_led_segments();
+        }
+
+        lit_led_segment(DP);
+    }
+    else if (axis == 'y')
+    {
+        if (!(on_digits & (1 << 2))) //check if digit-2 is turned off and then turn on to show dp
+        {
+            P2OUT = DIGIT_2;
+            turn_off_led_segments();
+        }
+
+        lit_led_segment(DP);
+    }
+    else if (axis == 'z')
+    {
+        if (!(on_digits & (1 << 1))) //check if digit-3 is turned off and then turn on to show dp
+        {
+            P2OUT = DIGIT_3;
+            turn_off_led_segments();
+        }
+
+        lit_led_segment(DP);
     }
 }
 
@@ -349,7 +369,7 @@ void lit_led_segment(LED_SEGMENTS segment)
 //Given a number to display, turns on the digits in display that need to be turned on
 //Then displays the digit, delaying an amount of time set in macros to improve persistence of vision
 //*******************************************************************************
-void display_digits(unsigned int val)
+void display_digits(unsigned int val, char axis)
 {
 
     unsigned int digit = 0;
@@ -359,22 +379,38 @@ void display_digits(unsigned int val)
     if (val >= 0 && val <= 9)
     {
         //If the number is less than 10, turn on first digit and display the value
+        digits_on = 1 << 0;
         digit = number % 10;
         P2OUT = DIGIT_4;
         led_display_num(digit);
+        _delay_cycles(DIGDELAY);
+        display_decimal_point(axis, digits_on);
     }
     else if (val >= 10 && val <= 99)
     {
         //If the number is more than 10, but less than 99
         //Turn on 1s place digit, display value, and delay
         //Then extract 10s place digit and repeat
+        digits_on = (1 << 0) + (1 << 1);
         digit = number % 10;
         P2OUT = DIGIT_4;
         led_display_num(digit);
-        digit = (number / 10) % 10;
         _delay_cycles(DIGDELAY);
+        digit = (number / 10) % 10;
         P2OUT = DIGIT_3;
         led_display_num(digit);
+        if (axis == 'z')
+        {
+            display_decimal_point('z', digits_on);
+        }
+        else if (axis == 'y')
+        {
+            display_decimal_point('y', digits_on);
+        }
+        else if (axis == 'x')
+        {
+            display_decimal_point('x', digits_on);
+        }
         _delay_cycles(DIGDELAY);
 
     }
@@ -384,23 +420,42 @@ void display_digits(unsigned int val)
         //Turn on 1s place digit, display value, and delay
         //Then extract 10s place digit and repeat
         //Then extract 1000s place digit and repeat again
+
+        digits_on = (1 << 0) + (1 << 1) + (1 << 2);
         digit = number % 10;
         P2OUT = DIGIT_4;
+        digits_on |= 1 << 0;
         led_display_num(digit);
         _delay_cycles(DIGDELAY);
         digit = (number / 10) % 10;
         P2OUT = DIGIT_3;
         led_display_num(digit);
+        if (axis == 'z')
+        {
+            display_decimal_point('z', digits_on);
+        }
         _delay_cycles(DIGDELAY);
         digit = (number / 100) % 10;
         P2OUT = DIGIT_2;
         led_display_num(digit);
+        if (axis == 'y')
+        {
+            display_decimal_point('y', digits_on);
+        }
+        else if (axis == 'x')
+        {
+            display_decimal_point('x', digits_on);
+//            P2OUT = DIGIT_1;
+//            turn_off_led_segments();
+//            lit_led_segment(DP);
+        }
         _delay_cycles(DIGDELAY);
 
     }
     else if (val >= 1000 && val <= 9999)
     {
         //Do the same process as above, but for all 4 digits.
+        digits_on = (1 << 0) + (1 << 1) + (1 << 2) + (1 << 3);
         digit = number % 10;
         P2OUT = DIGIT_4;
         led_display_num(digit);
@@ -408,16 +463,26 @@ void display_digits(unsigned int val)
         digit = (number / 10) % 10;
         P2OUT = DIGIT_3;
         led_display_num(digit);
+        if (axis == 'z')
+        {
+            display_decimal_point('z', digits_on);
+        }
         _delay_cycles(DIGDELAY);
         digit = (number / 100) % 10;
         P2OUT = DIGIT_2;
         led_display_num(digit);
-        _delay_cycles(10);
+        if (axis == 'y')
+        {
+            display_decimal_point('y', digits_on);
+        }
+        _delay_cycles(DIGDELAY);
         digit = (number / 1000) % 10;
         P2OUT = DIGIT_1;
         led_display_num(digit);
+        if (axis == 'x')
+        {
+            display_decimal_point('x', digits_on);
+        }
         _delay_cycles(DIGDELAY);
-
     }
-
 }
